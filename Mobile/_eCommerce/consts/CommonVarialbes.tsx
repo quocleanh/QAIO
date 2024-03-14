213d326acaaca7089888b08ad98a4d495d3d4cbe
Quốc Lê <quoc.leanh.001@gmail.com>
2024-03-14 13:25:53 +0700

    delete ecom tmp

--- Mobile/_eCommerce/consts/CommonVarialbes.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useWindowDimensions } from "react-native";
-const CommonVarialbes = {
-    ScreenWidth: useWindowDimensions(),
-    
-};
-
-export default CommonVarialbes;
--- Mobile/_eCommerce/data/detail/productRealted.tsx
+++
@@ -1,7 +0,0 @@
-const productRealted = [
-    { "Code": "162324APCAA", "Name": "Gạch Ốp Lát", "Type": 0, "Price": 1200000, "SalePrice": 1200000, "Image": "gach_op_lat_1jpg", "Description": "Gạch ốp lát chất lượng cao" },
-    { "Code": "162324APSTLP", "Name": "Gạch Ốp Lát", "Type": 0, "Price": 1299, "SalePrice": 1200000, "Image": "gach_op_lat_2jpg", "Description": "Gạch ốp lát kiểu mới" },
-    { "Code": "165TAVEGEDI", "Name": "Gạch Ốp Lát", "Type": 0, "Price": 899, "SalePrice": 1200000, "Image": "gach_op_lat_3jpg", "Description": "Gạch ốp lát thiết kế độc đáo" },
-    { "Code": "165TAVENE", "Name": "Gạch Ốp Lát", "Type": 0, "Price": 999, "SalePrice": 1200000, "Image": "gach_op_lat_4jpg", "Description": "Gạch ốp lát phong cách hiện đại" },
-  ];
-export default productRealted;
